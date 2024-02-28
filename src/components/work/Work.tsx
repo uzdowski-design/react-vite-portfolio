eff31ed003452041e86efe5ee396a35cb860a325
Michal Uzdowski <uzdowski.design@gmail.com>
2024-02-28 21:25:47 +0100

    fixed padding

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -3,7 +3,10 @@ import { workHistory } from '../../utils/constants';
 
 const Work = () => {
   return (
-    <section id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+    <section
+      id="work"
+      className="max-w-[1040px] m-auto md:pl-20 p-4 pb-16 pt-32"
+    >
       <h2 className="text-4xl font-bold text-center text-[#001b5e] mb-5">
         Work
       </h2>
