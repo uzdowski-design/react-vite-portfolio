2fe8cb573262e2bf83a877f71ba4315aa4c74ccb
Michal Uzdowski <uzdowski.design@gmail.com>
2024-02-28 22:25:17 +0100

    add contact section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Contact from './components/contact/Contact';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navigation/Navbar';
 import Projects from './components/projects/Projects';
@@ -13,6 +14,7 @@ function App() {
         <Hero />
         <Work />
         <Projects />
+        <Contact />
       </main>
     </div>
   );
---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,77 @@
+const Contact = () => {
+  return (
+    <section id="contact" className="max-w-[1040px] m-atuo md:pl-20 p-4 py-16">
+      <h2 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
+        Contact
+      </h2>
+      <form
+        action={import.meta.env.VITE_GETFORM_ENDPOINT}
+        method="POST"
+        encType="multipart/form-data"
+      >
+        <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+          <div className="flex flex-col">
+            <label className="uppercase text-sm py-2" htmlFor="name">
+              Name
+            </label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              className="border-2 rounded-lg p-3 flex border-gray-300"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="uppercase text-sm py-2" htmlFor="phone">
+              Phone
+            </label>
+            <input
+              id="phone"
+              type="tel"
+              name="phone"
+              className="border-2 rounded-lg p-3 flex border-gray-300"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col py-2">
+          <label className="uppercase text-sm py-2" htmlFor="email">
+            E-mail
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="border-2 rounded-lg p-3 flex border-gray-300"
+          />
+        </div>
+        <div className="flex flex-col py-2">
+          <label className="uppercase text-sm py-2" htmlFor="subject">
+            Subject
+          </label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            className="border-2 rounded-lg p-3 flex border-gray-300"
+          />
+        </div>
+        <div className="flex flex-col py-2">
+          <label className="uppercase text-sm py-2" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            rows={10}
+            id="message"
+            name="message"
+            className="border-2 rounded-lg p-3 border-gray-300"
+          />
+        </div>
+        <button className="bg-[#001b5e] text-gray-100 rounded-lg mt-4 w-full p-4">
+          Send Message
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
